8dbaa969c75df062af749f0a84d2827d59625c54
Joshua Galeria <jgaleriacs@gmail.com>
2022-04-21 11:40:20 -1000

    Implement methods for Node. Bring in Macros for Format Line Dump

--- Node.h
+++ Node.h
@@ -10,7 +10,67 @@
 ///////////////////////////////////////////////////////////////////////////////
 #pragma once
 
-class Node {
+#include <iostream>
+#include <iomanip>
+#include "programName.h"
 
+/// Format the heading for dumping members of a class to the console
+///
+/// Print =====================
+#define PRINT_HEADING_FOR_DUMP \
+ /* Print =========================================================== */ \
+ std::cout << std::setw(80) << std::setfill( '=' ) << "" << std::endl
+/// Format a line for dumping the members of a class to the console.
+/// Setup the fields for printing (space pad, left justify, etc.)
+#define FORMAT_LINE_FOR_DUMP( className, member ) \
+ std::cout << std::setfill( ' ' ) /* Space pad */ \
+ << std::left /* Left justify */ \
+ << std::boolalpha /* Print `true` or `false` for `bool`s */ \
+ << std::setw(8) << (className) \
+ << std::setw(20) << (member) \
+ << std::setw(52) /* (data) */
+
+class Node {
+    //So that other classes can access protected members
+    friend class List;
+    friend class SinglyLinkedList;
+
+    //Protected Members
+protected:
+    //Point to next node in the list
+    Node* next = nullptr;
+
+
+    //Public Methods
+    virtual void dump() const {
+        FORMAT_LINE_FOR_DUMP( "Node", "this") << this << std::endl;
+        FORMAT_LINE_FOR_DUMP( "Node", "next") << next << std::endl;
+    }
+
+    //Static Methods
+protected:
+    //Compare node1 greater than node 2
+    static bool compareAddress (const Node* node1, const Node* node2 ) {
+        if( node1 > node2 )
+            return true;
+        return false;
+    }
+
+    //Validate
+    virtual bool validate() const noexcept {
+        if( next == nullptr ) {
+            return true;
+        }
+        if( next == next-> next ) {
+            std::cout << PROGRAM_NAME << "There's a recursive loop, next is pointing to itself";
+            return false;
+        }
+    }
+
+    //Compare if left is greater than right
+    virtual bool operator>( const Node& rightSide ) {
+        return compareAddress( this, &(Node&)rightSide );
+    }
 };
+
 
